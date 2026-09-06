de3feb4b64cc5abe39aa1ef887729ae0adbc41a5
Pushkar N <131258869+Pnarayan-3@users.noreply.github.com>
2026-09-06 23:39:02 +0530

    Fixed Go Style

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -16,27 +16,33 @@ import (
 )
 
 func main() {
-	//load config
+	// load config
 	cfg := config.MustLoad()
-	//database setup
+
+	// database setup
 	storage, err := sqlite.New(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+	slog.Info("storage initialized",
+		slog.String("env", cfg.Env),
+		slog.String("version", "1.0.0"),
+	)
 
-	//setup router
+	// setup router
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /api/students", student.New(storage))
-	router.HandleFunc("GET /api/students/{id}",student.GetById(storage))
-		router.HandleFunc("GET /api/students",student.GetList(storage))
-	//setup server
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
+
+	// setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
+
 	slog.Info("server started %s", slog.String("address", cfg.Addr))
 
 	done := make(chan os.Signal, 1)
@@ -63,5 +69,4 @@ func main() {
 	}
 
 	slog.Info("server shutdown successfully")
-
 }
